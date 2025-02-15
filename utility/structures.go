baf552f82764c2a47e8a1e3489d1c3d89911e0b6
ervin <ervinsd@stud.ntnu.no>
2025-02-15 14:38:02 +0100

    Added few more structures

--- utility/structures.go
+++ utility/structures.go
@@ -11,17 +11,19 @@ type StatusStructur struct {
 }
 
 type CountryInfoStructure struct {
-	Name       string   `json:"name"`
-	Continents []string `json:"continents"`
-	Population int      `json:"population"`
-	Languages  struct {
-		Nno string `json:"nno"`
-		Nob string `json:"nob"`
-		Smi string `json:"smi"`
-	} `json:"languages"`
-
-	Borders []string `json:"borders"`
-	Flag    string   `json:"flag"`
-	Capital string   `json:"capital"`
-	Cities  []string `json:"cities"`
+	Name       string            `json:"name"`
+	Continents []string          `json:"continents"`
+	Population int               `json:"population"`
+	Languages  map[string]string `json:"languages"`
+	Borders    []string          `json:"borders"`
+	Flag       string            `json:"flag"`
+	Capital    string            `json:"capital"`
+	Cities     []string          `json:"cities"`
+}
+type PopulationStructur struct {
+	Mean   int `json:"mean"`
+	Values []struct {
+		Year  int `json:"year"`
+		Value int `json:"value"`
+	} `json:"values"`
 }
